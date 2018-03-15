d406f47ae0cf61329b87c4b68e78d8580d7011c9
xxh13 <15850781776@163.com>
2018-03-15 13:00:26 +0800

    java concurrency demo

---
+++ src/main/java/leetcode_120_140/_136.java
@@ -0,0 +1,35 @@
+package leetcode_120_140;
+
+import java.util.BitSet;
+import java.util.HashMap;
+import java.util.Map;
+
+public class _136 {
+
+//    public int singleNumber(int[] nums) {
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            if (!map.keySet().contains(nums[i])) {
+//                map.put(nums[i], 1);
+//            } else {
+//                map.put(nums[i], map.get(nums[i]) + 1);
+//            }
+//        }
+//
+//        for (Map.Entry entry : map.entrySet()) {
+//            if ((Integer) entry.getValue() == 1) {
+//                return (Integer) entry.getKey();
+//            }
+//        }
+//        return -1;
+//    }
+
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+
+        return result;
+    }
+}
