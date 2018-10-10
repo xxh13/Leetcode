d760fa333951dbb20e937069a43aa3d390a51108
xxh13 <1095279176893@tianchi.com>
2018-10-10 14:01:42 +0800

    update min stack

---
+++ src/main/java/leetcode_150_170/MinStack.java
@@ -0,0 +1,38 @@
+package leetcode_150_170;
+
+import java.util.Stack;
+
+public class MinStack {
+
+	private Stack<Integer> stack;
+	private Stack<Integer> minStack;
+
+	public MinStack() {
+		stack = new Stack<>();
+		minStack = new Stack<>();
+	}
+
+	public void push(int x) {
+		stack.push(x);
+		if (minStack.isEmpty() || x <= minStack.peek()) {
+			minStack.push(x);
+		}
+	}
+
+	public void pop() {
+		int top = stack.peek();
+		stack.pop();
+		if (top == minStack.peek()) {
+			minStack.pop();
+		}
+	}
+
+	public int top() {
+		return stack.peek();
+	}
+
+	public int getMin() {
+		return minStack.peek();
+	}
+
+}
