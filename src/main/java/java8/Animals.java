7f51d4a328db672e58d93e797e0f84d1b6601b4b
xxh13 <1095279176893@tianchi.com>
2018-10-13 17:35:28 +0800

    update 214

--- src/main/java/java8/Animals.java
+++ src/main/java/java8/Animals.java
@@ -5,7 +5,9 @@ import java.io.Serializable;
 public class Animals implements Serializable{
 
     private String name;
-    public Animals() {  }
+    public Animals() {
+        System.out.println("animals");
+    }
     public void setName(String name){
         this.name = name;
     }
--- src/main/java/java8/Persons.java
+++ src/main/java/java8/Persons.java
@@ -1,11 +1,20 @@
 package java8;
 
+import org.junit.Test;
+
 import java.io.Serializable;
 
 public class Persons extends Animals implements Serializable{
 
+	@Test
+	public void test() {
+		Persons persons = new Persons();
+	}
+
     private int age;
-    public Persons() {}
+    public Persons() {
+	    System.out.println("person");
+    }
     public void setAge(int age){
         this.age = age;
     }
